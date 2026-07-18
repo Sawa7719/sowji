1b10683301af0ad41f0f1c083f707d35d841daee
Sawa7719 <sarah.mzyene77@gmail.com>
2026-07-18 13:10:40 +0200

    Create lettres.js

---
+++ lettres.js
@@ -0,0 +1,75 @@
+/*
+=================================
+SOWJI
+Le coffre des lettres
+=================================
+*/
+
+
+const lettres = {
+
+
+"2026-07-21": {
+
+texte: `
+Ma chère Sowji,
+
+Le voyage commence aujourd'hui.
+
+Une première porte s'ouvre,
+et derrière elle se cache un chemin rempli
+de petits morceaux de bonheur.
+
+Avec toute mon affection.
+`,
+
+photo: "",
+
+video: "",
+
+musique: ""
+
+},
+
+
+
+"2026-07-22": {
+
+texte: `
+Une nouvelle journée apparaît.
+
+Une nouvelle lettre,
+un nouveau sourire,
+un nouveau souvenir à découvrir.
+`,
+
+photo: "",
+
+video: "",
+
+musique: ""
+
+},
+
+
+
+"2026-07-23": {
+
+texte: `
+
+Aujourd'hui encore,
+une petite partie du chemin
+vient jusqu'à toi.
+
+`,
+
+photo: "",
+
+video: "",
+
+musique: ""
+
+}
+
+
+};
